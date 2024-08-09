e311effcfe30abd6dd2c054cd7f3a5b318e75ada
Liz <66321798+Lyzzzi@users.noreply.github.com>
2024-08-09 11:35:21 -0400

    Main and helper scripts added

---
+++ appDeployV1.py
@@ -0,0 +1,12 @@
+"""
+@author Liz
+This is version 1 of the app as deployed on streamlit cloud.
+"""
+
+
+import streamlit as st
+from get_resultsDeployV1 import *
+
+st.write('place keeper app')
+
+writeSomethingElse()
---
+++ get_resultsDeployV1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def writeSomethingElse():
+    st.write('place keeper helper app')
